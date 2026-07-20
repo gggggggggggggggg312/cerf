77d5c55d497c5d7e502b705661af7fa4b581a2ec
Idk how to name my account <153889123+gggggggggggggggg312@users.noreply.github.com>
2026-07-20 12:29:59 +0700

    Refactor IpaqH3800 ASIC1 read/write methods

--- cerf/boards/ipaq_h3800/ipaq_h3800_asic1.cpp
+++ cerf/boards/ipaq_h3800/ipaq_h3800_asic1.cpp
@@ -16,45 +16,66 @@ void IpaqH3800Asic1::OnReady() {
 }
 
 uint8_t IpaqH3800Asic1::ReadByte(uint32_t addr) {
-    const uint32_t off = addr - MmioBase();
-    return static_cast<uint8_t>(
-        (gpio_out_.load(std::memory_order_acquire) >> (8 * off)) & 0xFFu);
+    return static_cast<uint8_t>(ReadWord(addr & ~3u) >> ((addr & 3u) * 8));
 }
 
 uint32_t IpaqH3800Asic1::ReadWord(uint32_t addr) {
-    LOG(Periph, "[H3800 ASIC1] R32 %08X\n", addr);
-    return gpio_out_.load(std::memory_order_acquire);
+    const uint32_t off = addr - kAsic1Base;
+
+    switch (off) {
+    case kGpioOutOffset:  return gpio_out_.load(std::memory_order_acquire);
+    case kGpioDirOffset:  return gpio_dir_.load(std::memory_order_acquire);
+    case kGpioMaskOffset: return gpio_mask_.load(std::memory_order_acquire);
+    default:
+        LOG(Periph, "[H3800 ASIC1] unknown R32 %08X\n", off);
+        return 0;
+    }
 }
 
 void IpaqH3800Asic1::WriteByte(uint32_t addr, uint8_t value) {
-    const uint32_t off   = addr - MmioBase();
-    const uint32_t shift = 8 * off;
-    const uint32_t mask  = 0xFFu << shift;
-
-    uint32_t before = gpio_out_.load(std::memory_order_acquire);
-    uint32_t after  = (before & ~mask) |
-                      (static_cast<uint32_t>(value) << shift);
-
-    gpio_out_.store(after, std::memory_order_release);
-
-    LOG(Periph, "[H3800 ASIC1] W8 +%u = %02X -> %08X\n",
-        off, value, after);
+    const uint32_t aligned = addr & ~3u;
+    const uint32_t shift   = (addr & 3u) * 8;
+    uint32_t cur = ReadWord(aligned);
+    cur = (cur & ~(0xFFu << shift)) | (static_cast<uint32_t>(value) << shift);
+    WriteWord(aligned, cur);
 }
 
 void IpaqH3800Asic1::WriteWord(uint32_t addr, uint32_t value) {
-    gpio_out_.store(value, std::memory_order_release);
+    const uint32_t off = addr - kAsic1Base;
 
-    LOG(Periph, "[H3800 ASIC1] W32 %08X = %08X\n", addr, value);
+    switch (off) {
+    case kGpioOutOffset:
+        gpio_out_.store(value, std::memory_order_release);
+        LOG(Periph, "[H3800 ASIC1] GPIO_OUT = %08X\n", value);
+        break;
+
+    case kGpioDirOffset:
+        gpio_dir_.store(value, std::memory_order_release);
+        LOG(Periph, "[H3800 ASIC1] GPIO_DIR = %08X\n", value);
+        break;
+
+    case kGpioMaskOffset:
+        gpio_mask_.store(value, std::memory_order_release);
+        LOG(Periph, "[H3800 ASIC1] GPIO_MASK = %08X\n", value);
+        break;
+
+    default:
+        LOG(Periph, "[H3800 ASIC1] unknown W32 %08X = %08X\n", off, value);
+        break;
+    }
 }
 
 void IpaqH3800Asic1::SaveState(StateWriter& w) {
     w.Write(gpio_out_.load(std::memory_order_acquire));
+    w.Write(gpio_dir_.load(std::memory_order_acquire));
+    w.Write(gpio_mask_.load(std::memory_order_acquire));
 }
 
 void IpaqH3800Asic1::RestoreState(StateReader& r) {
-    uint32_t v = 0;
-    r.Read(v);
-    gpio_out_.store(v, std::memory_order_release);
+    uint32_t v;
+    r.Read(v); gpio_out_.store(v, std::memory_order_release);
+    r.Read(v); gpio_dir_.store(v, std::memory_order_release);
+    r.Read(v); gpio_mask_.store(v, std::memory_order_release);
 }
 
 REGISTER_SERVICE(IpaqH3800Asic1);
